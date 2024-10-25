358255753a5c179ca009c9e236ef637c5e4f7616
kesompochy <kesompochy@gmail.com>
2024-10-25 14:07:37 +0900

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="beametrics",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "apache-beam[gcp]>=2.60.0",
